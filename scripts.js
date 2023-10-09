b5a99ff5d9537d62822f5ac6ed2e5b282d128fb1
Alexander Heras <alexander.heras@hotmail.com>
2023-10-09 19:17:05 +0200

    Optimized contact page, gallery, added footer and updated homepage with more media queries in order to handle hero section easier

--- scripts.js
+++ scripts.js
@@ -6,3 +6,13 @@ function activateHamburger() {
     x.style.display = "block";
   }
 }
+
+function myFunction(x) {
+  if (y.matches) { // If media query matches
+    document.getElementById("nav-menu").removeAttribute("style");
+  } 
+}
+
+var y = window.matchMedia("(min-width: 35rem)")
+myFunction(y) // Call listener function at run time
+y.addListener(myFunction) // Attach listener function on state changes
